f375637aa66e0fc5e5cdcbf8be598ab8b10c1cb2
Ilya Lysenko <ilysenko.dev@gmail.com>
2025-07-24 16:14:10 +0400

    Add type conversion example from string to float

--- Lesson1.py
+++ Lesson1.py
@@ -7,3 +7,7 @@ print(name, age, height, programmer)
 current_year = 2025
 birth_year = current_year - age
 print("Your birth year is: ", birth_year)
+
+weight_string = "70.5"
+weight = float(weight_string)
+print(weight)
